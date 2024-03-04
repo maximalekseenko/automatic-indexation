a6e35ad742cf34d55427b6b109e08625a11e597e
maximalekseenko <max.alekseenko@gmail.com>
2024-03-04 21:15:08 +0300

    Settings added

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Settings {
+    class Settings {
+        public static string inputFilePath="infile.json";
+        public static string outputFilePath="outfile.json";
+        public static string indexKey="$id";
+    }
+}
